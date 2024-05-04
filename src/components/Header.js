9447d59d1a47c6b4b82b5aac86eba1069ef8efc1
I=m-abhi <abhisasa2001@gmail.com>
2024-05-04 14:20:40 +0530

    Setup upto Main-Container

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import NetflixLogo from '../utils/Netflix_Logo_PMS.png';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { addUser, removeUser } from '../utils/userSlice';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate('/');
+    }).catch((error) => {
+      // An error happened.
+      navigate('/error');
+    });
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
+        navigate("/browse");
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
+      <img className='w-36' src={NetflixLogo} alt="logo" />
+      {user && (
+        <div className="flex p-2">
+          <img className='w-8' src={user?.photoURL} alt="userIcon" />
+          <button onClick={handleSignOut} className="font-bold text-black">(Sign Out)</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2M5N2RjMDk2M2U0MzEzYmE0OGQ5NDA4MzQ3YjI5MSIsInN1YiI6IjY2MzBiN2FlMjQ3NmYyMDEyYmZiYmJkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p-qnOh7183Q5dt0G01C0dr1ENte9fKnnUT7RvKJaZX0'
+  }
+};
